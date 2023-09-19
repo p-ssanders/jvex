6a24140cc6c0f964a0404ad410fcbbda1a2d4467
Sam Sanders <hello@samsanders.dev>
2023-09-19 10:24:47 -0600

    enrich document domain: author cannot be null (adding test coverage)

--- src/test/java/dev/samsanders/openvex/DocumentTest.java
+++ src/test/java/dev/samsanders/openvex/DocumentTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import java.net.URI;
 
+import static dev.samsanders.openvex.Document.DEFAULT_CONTEXT;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class DocumentTest {
@@ -12,6 +13,13 @@ class DocumentTest {
     void context_cannot_be_null() {
         assertThrows(IllegalArgumentException.class, () -> {
             new Document(null, URI.create("http://some.uri"), "some author");
+        });
+    }
+
+    @Test
+    void author_cannot_be_null() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Document(DEFAULT_CONTEXT, URI.create("http://some.uri"), null);
         });
     }
 
